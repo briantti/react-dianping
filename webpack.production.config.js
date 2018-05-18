db45c2e0da70c40239ae63faf8b80e8b2c9fbcc9
Briantti <AnneLB@foxmail.com>
2018-05-18 10:15:16 +0800

    [feature]修改webpack.production.config.js的[chunkhash:8]为[sha512:hash:base64:8]才不报错，生成build打包后的文件

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -36,11 +36,11 @@ module.exports = {
             },
             {
                 test: /\.(png|gif|jpg|jpeg|bmp)$/i,
-                loader: 'url-loader?limit=5000&name=img/[name].[chunkhash:8].[ext]'
+                loader: 'url-loader?limit=5000&name=img/[name].[sha512:hash:base64:8].[ext]'
             },  // 限制大小5kb
             {
                 test: /\.(png|woff|woff2|svg|ttf|eot)($|\?)/i,
-                loader: 'url-loader?limit=5000&name=fonts/[name].[chunkhash:8].[ext]'
+                loader: 'url-loader?limit=5000&name=fonts/[name].[sha512:hash:base64:8].[ext]'
             },// 限制大小小于5k
         ]
     },
